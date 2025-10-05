7e51a49751c6b8fc0572170f147ed8034c8a2e45
Er-Prashant-Developer <vprashant165@gmail.com>
2025-10-05 20:28:34 +0530

    completed

--- script.js
+++ script.js
@@ -30,9 +30,7 @@ ScrollTrigger.refresh();
 
 }
 
-locomotiveAnimation()
-
-function loadingAnimation(){
+function loadingAnimation(){ 
   var t1=gsap.timeline();
 
 t1.from(".line h1",{
@@ -56,7 +54,7 @@ h5timer.innerHTML=grow++;
   }else{
     h5timer.innerHTML=grow;
   }
-},30)
+},28)
   }
 })
 
@@ -96,18 +94,148 @@ t1.from("#hero1, #page2",{
 },"-=1.2");
 
 }
-loadingAnimation();
 
 function cursorAnimation(){
-  document.addEventListener("mousemove",function(dets){
-gsap.to("#crsr",{
-  left:dets.x,
-  top:dets.y
+//   document.addEventListener("mousemove",function(dets){
+// gsap.to("#crsr",{
+//   left:dets.x,
+//   top:dets.y
+// })
+// })
+
+Shery.mouseFollower({
+  skew:true,
+  ease:"cubic-bezier(0.23, 1, 0.320, 1)",
+  duration:1,
+});
+Shery.makeMagnet("#nav-part2 h4");
+
+var videoContainer=document.querySelector("#video-container");
+
+var video=document.querySelector("#video-container video");
+
+
+videoContainer.addEventListener("mouseenter",function(){
+ videoContainer.addEventListener("mousemove",function(dets){
+  gsap.to(".mousefollower",{
+   opacity:0
+  })
+gsap.to("#video-cursor",{
+  left:dets.clientX -470,
+  y:dets.clientY -250
 })
+ })
 })
 
-Shery.makeMagnet("#nav-part2 h4");
+videoContainer.addEventListener("mouseleave",function(){
+  gsap.to(".mousefollower",{
+    opacity:1
+  })
+  gsap.to("#video-cursor",{
+    left:"70%",
+    top:"-15%"
+  })
+})
+
+let flag=0;
+videoContainer.addEventListener("click",function(){
+ 
+ if(flag==0){
+video.play();
+  video.style.opacity=1
+  document.querySelector("#video-cursor").innerHTML='<i class="ri-pause-mini-fill"></i>'
+
+  gsap.to("#video-cursor",{
+    scale:0.5
+  })
+  flag=1;
+ }else{
+video.pause();
+  video.style.opacity=1
+
+  document.querySelector("#video-cursor").innerHTML='<i class="ri-play-mini-fill"></i>'
+
+  gsap.to("#video-cursor",{
+    scale:1
+  })
+  flag=0;
+ }
+  
+})
+
 }
 
-// cursorAnimation();
+function sheryAnimation(){
+  Shery.imageEffect(".image-div",{
+    style:5,
+    // debug:true,
+    config:{"a":{"value":1.6,"range":[0,30]},"b":{"value":-0.97,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7241195605115471},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.43,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":2.98,"range":[0,10]},"metaball":{"value":0.47,"range":[0,2]},"discard_threshold":{"value":0.49,"range":[0,1]},"antialias_threshold":{"value":0.01,"range":[0,0.1]},"noise_height":{"value":0.34,"range":[0,2]},"noise_scale":{"value":20.61,"range":[0,100]}},
+    gooey:true
+  })
+}
+
+function FlagAnimationHero(){
+document.addEventListener("mousemove",function(dets){
+gsap.to("#flag",{
+  x:dets.x,
+  y:dets.y
+})
+})
+
+document.querySelector("#hero3").addEventListener("mouseenter",function(){
+  gsap.to("#flag",{
+    opacity:1,
+  })
+})
+
+document.querySelector("#hero3").addEventListener("mouseleave",function(){
+  gsap.to("#flag",{
+    opacity:0,
+  })
+})}
+
+function textillatejs(){
+  //autoplay dont start without hover
+$('#footer h1').textillate({
+  autostart:false,
+  in:{effect :"fadeIn", delay:50},
+ out:{effect :"fadeIn", delay:50 ,duration:2}
+})
+
+  let h1=document.querySelector("#footer h1")
+
+  console.log(h1)
+  //mouse enter code
+
+  h1.addEventListener("mouseenter",function(){
+  h1.style.webkitTextStroke='2px #fff'
+  h1.style.color='tansparent'
+   h1.style.fontStyle='italic'
+    gsap.from("h1",{
+  
+onStart:function(){
+$('h1').textillate('in')
+}
+    })
+  })
+
+//mouse leave code
+   h1.addEventListener("mouseleave",function(){
+    h1.style.webkitTextStroke='0.5px white'
+   h1.style.color='white'
+   h1.style.fontStyle='normal'
+     gsap.from("h1",{
+onStart:function(){
+$('h1').textillate('out')
+},
+})
+  })
+}
+
+loadingAnimation();
+cursorAnimation();
+locomotiveAnimation();
+sheryAnimation();
+FlagAnimationHero();
+textillatejs();
 
